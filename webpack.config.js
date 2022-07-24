bc9f60519e058196a095e25b34c78314b3435a14
niyi <31526995+halloweencai@users.noreply.github.com>
2022-07-24 10:27:11 +0800

    样式处理

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,89 @@
 // webpack是node写出来的，要用node写法
 let path = require('path')  // 内置模块，直接引用
 let HtmlWebpackPlugin = require('html-webpack-plugin')
+let MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
-    devServer: {
-        port: 3000,
-        compress: true, // 压缩
-        // webpack5好像一定要配置
-        static: {
-            directory: path.join(__dirname, './dist')
-        }
-    },
-    mode: 'production', // 模式，默认两种，production和development
+    // devServer: {
+    //     port: 3000,  // 端口号配置
+    //     compress: true, // 开启gzip压缩
+    //     // webpack5好像一定要配置
+    //     static: {
+    //         directory: path.join(__dirname, './dist')
+    //     }
+    // },
+    mode: 'development', // 模式，默认两种，production和development
     entry: './src/index.js', // 入口
     output: {
         filename: 'bundle.[hash:8].js',  // 打包后的文件名
-        path: path.resolve(__dirname, 'dist')  // __dirname代表当前目录下, 路径必须是一个绝对路径
+        path: path.resolve(__dirname, 'dist')  // __dirname（可以不加）代表当前目录下, 路径必须是一个绝对路径
     },
     plugins: [  // 数组 放着所有的webpack插件
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
             minify: {
-                // 去掉双引号
+                // 去掉属性双引号
                 removeAttributeQuotes: true,
-                // 去掉空格
+                // 折叠空格，变成一行
                 collapseWhitespace: true
             },
             // 加上hash戳
             hash: true
         })
-    ]
+    ],
+    module: { // 模块
+        // loader
+        rules: [ // 规则 css-loader接续@import这种语法的
+            // style-loader 它是把css 插入到head标签中
+            // loader的特点， 希望单一
+            // loader的用法，字符串只用一个loader
+            // 多个loader需要[]
+            // loader的顺序，默认是从右向左执行，从下到上执行
+            // loader还可以写成对象方式，多写点配置
+            {
+                // 处理css
+                test: /\.css$/, 
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            // insertAt: 'top'  // css的样式插入到顶部，避免head中的style被覆盖
+                            // insertAt 已废弃
+                            insert: () => {
+
+                            }
+                        }
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                // 处理less   sass -> node-sass sass-loader
+                // stylus -> stylus-loader
+                test: /\.less$/, 
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insert: function(element) {
+                                var parent = document.querySelector('head');
+                                var lastInsertedElement = window._lastElementInsertedByStyleLoader;
+                                if (!lastInsertedElement) {
+                                    parent.insertBefore(element, parent.firstChild);
+                                } else if (lastInsertedElement.nextSibling) {
+                                    parent.insertBefore(element, lastInsertedElement.nextSibling)
+                                } else {
+                                    parent.appendChild(element)
+                                }
+    
+                            }
+                        }
+                    },
+                    'css-loader',  // @import 解析路径
+                    'less-loader' // 把less -> css
+                ]
+            }
+        ]
+        
+    }
 }
